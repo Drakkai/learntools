7789b364becf7676ae8e82bb7dd9631c841fc2c8
Maria Nicula <maria.nicula@ing.com>
2020-01-22 10:48:47 +0200

    Updated ex1

--- learntools/python/ex1.py
+++ learntools/python/ex1.py
@@ -7,33 +7,6 @@ class ExerciseFormatTutorial(EqualityCheckProblem):
     _hint = "Your favorite color rhymes with *glue*."
     _solution = CS('color = "blue"')
 
-    @property
-    def _correct_message(self):
-        history = self._view.interactions
-        if history['hint'] == 0 and history['solution'] == 0:
-            return ("What?! You got it right without needing a hint or anything?"
-                    " Drats. Well hey, you should still continue to the next step"
-                    " to get some practice asking for a hint and checking solutions."
-                    " (Even though you obviously don't need any help here.)"
-                    )
-        return ''
-
-    def _failure_message(self, var, actual, expected):
-        if (
-                any(actual.endswith(suff) for suff in ['oo', 'ue', 'ew'])
-                and actual.strip().lower() != 'blue'
-            ):
-            return "Ha ha, very funny."
-        elif actual.strip(' .!').lower() == 'ni':
-            return "Please! Please! No more! We will find you a shrubbery."
-        return ("{} is not your favorite color!"
-                " Well, maybe it is, but we're writing the rules. The point"
-                " of this question is to force you to get some practice asking"
-                " for a hint. Go ahead and uncomment the call to `q0.hint()`"
-                " in the code cell below, for a hint at what your favorite color"
-                " *really* is.").format(actual)
-
-
 class CircleArea(EqualityCheckProblem):
     _vars = ['radius', 'area']
     _expected = [3/2, (3/2)**2 * 3.14159]
