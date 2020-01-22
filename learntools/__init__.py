d16730b6a796cacf768a512c87c3212980db7e62
Maria Nicula <maria.nicula@ing.com>
2020-01-22 09:53:39 +0200

    ML challenge week 1

--- learntools/__init__.py
+++ learntools/__init__.py
@@ -1,5 +1,3 @@
-from . import core, data_viz_to_coder, deep_learning, embeddings, gans, \
-              machine_learning, ml_explainability, ml_insights, ml_intermediate, python, \
-              sql, ml_challenge
+from . import core, challenge_week1
 
 __version__ = '0.3.4'
